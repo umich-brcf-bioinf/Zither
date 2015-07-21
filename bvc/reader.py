5c6b26e7757ec6f77c35f656d5d71ece3139cafb
dkriti <dkriti@bfx-comp3.ccmb.med.umich.edu>
2015-07-21 17:05:09 -0400

    (cgates/dkriti): Simplified code and added tests

--- bvc/reader.py
+++ bvc/reader.py
@@ -6,22 +6,22 @@ class Reader(object):
         self.samfile = pysam.AlignmentFile(sam_file_name, "rb" )
 
     def get_depth_and_alt_freq(self, chrom, position_one_based, ref, alt):
+        BASE_INDEX = {'A':0, 'C':1, 'G':2, 'T':3}        
         alt = alt.upper()
-        total_depth = 0
-        total_AF = 0
         AF = 0
         position_zero_based = position_one_based - 1
+        coverage = self.samfile.count_coverage(chr=chrom,
+                                          start=position_zero_based,
+                                          stop=position_one_based,
+                                          quality_threshold=-1,
+                                          read_callback='nofilter')
         
-        for pileupcolumn in self.samfile.pileup(chrom, position_zero_based, position_one_based):
-            if (pileupcolumn.reference_pos == position_zero_based):
-                for pileupread in pileupcolumn.pileups:
-                    if not pileupread.is_del and not pileupread.is_refskip:
-                        total_depth += 1
+        total_depth = coverage[0][0] + coverage[1][0] + coverage[2][0]+ coverage[3][0]
+        try:
+            variant_count = coverage[BASE_INDEX[alt]][0]
+            if total_depth and len(ref)==1:
+                AF = variant_count/total_depth
+        except KeyError:
+            pass #AF remains 0
 
-                        if len(ref)==1 and pileupread.alignment.query_sequence[pileupread.query_position].upper() == alt:
-                            total_AF += 1
-
-                break
-        if total_depth:
-            AF = total_AF/total_depth
         return (total_depth, AF)
--- test/reader_test.py
+++ test/reader_test.py
@@ -6,6 +6,32 @@ from testfixtures import TempDirectory
 import unittest
 from subprocess import call
 
+class _SamFlag(object): 
+    #the read is paired in sequencing, no matter whether it is mapped in a pair
+    PAIRED = 1 
+    # the read is mapped in a proper pair
+    PROPER_PAIR = 2
+    #the read itself is unmapped; conflictive with PROPER_PAIR 
+    UNMAP = 4
+    #the mate is unmapped 
+    MUNMAP = 8
+    #the read is mapped to the reverse strand 
+    REVERSE = 16
+    #the mate is mapped to the reverse strand 
+    MREVERSE = 32
+    #this is read1 
+    READ1 = 64
+    #this is read2 
+    READ2 = 128
+    #not primary alignment 
+    SECONDARY = 256
+    #QC failure 
+    QCFAIL = 512
+    #optical or PCR duplicate 
+    DUP = 1024
+    #supplementary alignment 
+    SUPPLEMENTARY = 2048
+
 def _create_bam(dir, sam_contents):
     test_input_sam = os.path.join(dir, "test.sam")
     sam_file = open(test_input_sam, "w")
@@ -17,6 +43,9 @@ def _create_bam(dir, sam_contents):
     return test_input_bam
 
 class ReaderTestCase(unittest.TestCase):
+    def setUp(self):
+        self.simple_forward_read = _SamFlag.PAIRED + _SamFlag.PROPER_PAIR + _SamFlag.MREVERSE + _SamFlag.READ1
+
     def test_get_depth_and_alt_freq(self):
         sam_contents = \
 '''@HD	VN:1.4	GO:none	SO:coordinate
@@ -113,4 +142,58 @@ HWI-D00709:21:C6FJGANXX:1:2214:16769:34260	147	chr10	7	0	4M	=	105	0	TTTT	>>>>
             self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=9, ref="C", alt="A")[0])
             self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=10, ref="C", alt="A")[0])
 
+
+    def test_get_depth_and_alt_freq_ignoresBaseCallQuality(self):
+        sam_contents = \
+'''@HD	VN:1.4	GO:none	SO:coordinate
+@SQ	SN:chr10	LN:135534747
+HWI-D00709:21:C6FJGANXX:1:2214:16769:34260	99	chr10	5	0	5M	=	105	0	AAAAA	50+&!
+'''
+        #Q  ASCII
+        #0  !
+        #5  &
+        #10 +
+        #15 0
+        #20 5
+        with TempDirectory() as tmp_dir:    
+            input_bam = _create_bam(tmp_dir.path, sam_contents)
+            reader = bvc_reader.Reader(input_bam)
+            self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=5, ref="C", alt="A")[0])
+            self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=6, ref="C", alt="A")[0])
+            self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=7, ref="C", alt="A")[0])
+            self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=8, ref="C", alt="A")[0])
+            self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=9, ref="C", alt="A")[0])
+
+    def test_get_depth_and_alt_freq_duplicatesCounted(self):
+        
+        sam_contents = \
+'''@HD	VN:1.4	GO:none	SO:coordinate
+@SQ	SN:chr10	LN:135534747
+HWI-D00709:21:C6FJGANXX:1:2214:16769:34260	{}	chr10	5	0	5M	=	105	0	AAAAA	>>>>>
+'''.format(self.simple_forward_read + _SamFlag.DUP)
+        with TempDirectory() as tmp_dir:    
+            input_bam = _create_bam(tmp_dir.path, sam_contents)
+            reader = bvc_reader.Reader(input_bam)
+            self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=5, ref="C", alt="A")[0])
             
+    def test_get_depth_and_alt_freq_failedQualityCounted(self):
+        sam_contents = \
+'''@HD	VN:1.4	GO:none	SO:coordinate
+@SQ	SN:chr10	LN:135534747
+HWI-D00709:21:C6FJGANXX:1:2214:16769:34260	{}	chr10	5	0	5M	=	105	0	AAAAA	>>>>>
+'''.format(self.simple_forward_read + _SamFlag.QCFAIL)
+        with TempDirectory() as tmp_dir:    
+            input_bam = _create_bam(tmp_dir.path, sam_contents)
+            reader = bvc_reader.Reader(input_bam)
+            self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=5, ref="C", alt="A")[0])
+            
+    def test_get_depth_and_alt_freq_secondaryReadsCounted(self):
+        sam_contents = \
+'''@HD	VN:1.4	GO:none	SO:coordinate
+@SQ	SN:chr10	LN:135534747
+HWI-D00709:21:C6FJGANXX:1:2214:16769:34260	{}	chr10	5	0	5M	=	105	0	AAAAA	>>>>>
+'''.format(self.simple_forward_read + _SamFlag.SECONDARY)
+        with TempDirectory() as tmp_dir:    
+            input_bam = _create_bam(tmp_dir.path, sam_contents)
+            reader = bvc_reader.Reader(input_bam)
+            self.assertEquals(1, reader.get_depth_and_alt_freq(chrom="chr10", position_one_based=5, ref="C", alt="A")[0])
